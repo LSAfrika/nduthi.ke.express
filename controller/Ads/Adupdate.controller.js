f5df4db6d8e06140f29524429fc63ee112d18851
LSAfrika <lstudiosafrika@gmail.com>
2022-11-05 13:33:11 +0300

    finalized image deletion and update

--- controller/Ads/Adupdate.controller.js
+++ controller/Ads/Adupdate.controller.js
@@ -1,7 +1,6 @@
 const Admodel = require("../../models/Advert.model");
 const fs = require("fs");
 
-
 exports.updatead = async (req, res) => {
   try {
     id = req.params.id;
@@ -41,89 +40,97 @@ exports.imagesupdate = async (req, res) => {
 
   const ad = await Admodel.findById(id);
   // const ad = await Admodel.findById(id).exists();
-if(ad===null) return res.status(400).send({message:'no ad found'})
-  let deletionarray=[]
-  if (Object.keys(req.body).length !== 0){
-let counter=0
+  if (ad === null) return res.status(400).send({ message: "no ad found" });
+  let deletionarray = [];
+  if (Object.keys(req.body).length !== 0) {
+    let counter = 0;
     console.log("full image path for deletion:\n", req.body.del);
 
-    if(Array.isArray(req.body.del)===true){
- 
-    const imagespathfordeletionarray= req.body.del
-    const imagesfordeletionarraylength= req.body.del.length
-    console.log('created array for images',imagespathfordeletionarray);
+    if (Array.isArray(req.body.del) === true) {
+      const imagespathfordeletionarray = req.body.del;
+      const imagesfordeletionarraylength = req.body.del.length;
+      console.log("created array for images", imagespathfordeletionarray);
+      let arrayForImagesToCompareForDeletion = [];
+      imagespathfordeletionarray.forEach((path) => {
+        arrayForImagesToCompareForDeletion.push(path);
+        const segmentedpath = path.split("adimages")[1];
+        const filepath = "public/adimages" + segmentedpath;
+        deletionarray.push(filepath);
 
-    imagespathfordeletionarray.forEach(path => {
-      const segmentedpath = path.split('adimages')[1]
-      const filepath='public/adimages'+segmentedpath
-      deletionarray.push(filepath)
-      counter++
+        counter++;
 
-      if(counter>=imagesfordeletionarraylength) {
-        console.log('image path for deletion: ',deletionarray)
+        if (counter >= imagesfordeletionarraylength) {
+          const imagesInAd = ad.images;
+          console.log("images links in ad before deletion: ", ad.images);
 
-        console.log('multiple images array before splicing: ',ad.images);
+          deletionarray.forEach((del) => {
+            try {
+              fs.unlinkSync(del);
+            } catch (error) {
+              console.log("error when deletingimages: \n", error.message);
+              throw new Error(error.message);
 
-        deletionarray.forEach(del=>{
-          try {
-             fs.unlinkSync(del)
-            let pathindex=  ad.images.indexOf(path)
-      ad.images.splice(pathindex,1)
-      console.log('multiple new images array after splicing: ',ad.images);
-      
-            
-          } catch (error) {
-            return res.send({message:'error while deleting images',imageerror:error.message})
-          }
-        })
+              // return res.send({
+              //   message: "error while deleting images",
+              //   imageerror: error.message,
+              // });
+            }
+          });
 
-        
-     
-      
+          const newImagesArrayToSave = imagesInAd.filter((imageslink) => {
+            return !arrayForImagesToCompareForDeletion.find((deletionlink) => {
+              return imageslink === deletionlink;
+            });
+          });
+
+          ad.images = [];
+          ad.images = newImagesArrayToSave;
+
+          console.log("images links in ad after deletion: ", ad.images);
+        }
+      });
+      // return
+    } else {
+      const fullpath = req.body.del;
+      const segmentedpath = fullpath.split("adimages")[1];
+      const filepath = "public/adimages" + segmentedpath;
+
+      try {
+        fs.unlinkSync(filepath);
+        let pathindex = ad.images.indexOf(fullpath);
+        console.log("single image array before splicing: ", ad.images);
+        ad.images.splice(pathindex, 1);
+        console.log("single new image array after splicing: ", ad.images);
+      } catch (error) {
+        return res.send({
+          message: "error while deleting images",
+          imageerror: error.message,
+        });
       }
-    });
-    // return
-
-  }else{
-    const fullpath= req.body.del
-    const segmentedpath =fullpath.split('adimages')[1]
-    const filepath='public/adimages'+segmentedpath
-
-    try {
-       fs.unlinkSync(filepath)
-      let pathindex=  ad.images.indexOf(fullpath)
-      console.log('single image array before splicing: ',ad.images);
-      ad.images.splice(pathindex,1)
-      console.log('single new image array after splicing: ',ad.images);
-      
-    } catch (error) {
-      return res.send({message:'error while deleting images',imageerror:error.message})
     }
+
+    await ad.save();
+
+    // console.log("ad after images deletion\n", ad);
+    console.log("images links in ad after deletion: ", ad.images);
   }
 
-  await ad.save()
+  // return
 
-
-}
-
-// return
-
-
- // return res.send({message:'testing images working',array:deletionarray,deletedimages:ad})
+  // return res.send({message:'testing images working',array:deletionarray,deletedimages:ad})
 
   // const ad = await Admodel.findById('62ee610e9e3146f88ac294b1');
 
-  
-  const dirpath=`adimages/${id}`
-  let imagecounter=0
+  const dirpath = `adimages/${id}`;
+  let imagecounter = 0;
 
   if (req.files) {
     //  console.log("files in body:\n", req.files);
 
-     const imgobject = req.files;
-     const imgentries = Object.keys(imgobject);
-     const imageslength = imgentries.length;
-     imgentries.forEach(async (img) => {
+    const imgobject = req.files;
+    const imgentries = Object.keys(imgobject);
+    const imageslength = imgentries.length;
+    imgentries.forEach(async (img) => {
       let imagename = Date.now() + imgobject[img].name;
       let filepath = dirpath + "/" + imagename;
       let savepath = "public/" + filepath;
@@ -148,13 +155,14 @@ let counter=0
 
     console.log("images: ", ad.images);
 
-    if(imagecounter>=imageslength) await ad.save()
+    if (imagecounter >= imageslength) await ad.save();
 
-    return res.status(202).send({ message: "ad images updated",updatedad:ad });
+    return res
+      .status(202)
+      .send({ message: "ad images updated", updatedad: ad });
   }
 
-  return res.status(202).send({ message: "ad update complete",updatedad:ad });
-
+  return res.status(202).send({ message: "ad update complete", updatedad: ad });
 
   // const updatead = Admodel.findById(id);
   // const savepath = ``;
